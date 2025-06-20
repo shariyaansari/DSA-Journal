55ce2becfe3473085ced8345677c7214a3ae1ccf
shariyaansari <shariyaansari8425@gmail.com>
2025-06-20 22:27:06 +0530

    On recursion Basics right now

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+public class Fibonacci {
+    public static void fib(int n){
+        int a = 0;
+        int b = 1;
+        int c = a+b;
+        for(int i = 3; i<=n;i++){
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        System.out.println(c);
+    }
+    public static int fibWithRecursion(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return fibWithRecursion(n-1) + fibWithRecursion(n-2);
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        // fib(8);
+        System.out.println(fibWithRecursion(0));
+    }
+}

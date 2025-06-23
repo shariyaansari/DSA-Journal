e1f5e7fa5fa9715c55ca04cee7f48b97aa87e51f
shariyaansari <shariyaansari8425@gmail.com>
2025-06-23 15:58:46 +0530

    Did Container with Most water both approaches

---
+++ Arrays/ConatinerWithMaxWater.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class ConatinerWithMaxWater {
+    public static int maxWater(int height[]){
+        int min = 0; 
+        int maxArea = 0;
+        for(int i = 0;i < height.length;i++){
+            for(int j = i+1; j < height.length;j++ ){
+                maxArea = Math.max(maxArea, Math.min(height[i], height[j])*(j - i));
+            }
+        }
+        return maxArea;
+    }
+    public static int Optimized(int height[]){
+        int left = 0; 
+        int right = height.length -1;
+        int maxArea = 0;
+        while(left < right){
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right])*(right - left));
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        int height[] = {1,2,4,3};
+        System.out.println(Optimized(height));
+    }
+}

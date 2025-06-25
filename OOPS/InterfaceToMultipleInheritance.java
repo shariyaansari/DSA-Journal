5f4ddc34fbde6f519d151b361c53c2aef0b89297
shariyaansari <shariyaansari8425@gmail.com>
2025-06-25 22:38:10 +0530

    Completed Inheritance with these topics - will practice questions from now on

---
+++ OOPS/InterfaceToMultipleInheritance.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class InterfaceToMultipleInheritance {
+    public static void main(String[] args) {
+        Bear myBear = new Bear();
+        myBear.type();
+    }
+}
+
+interface Herbivore {
+    void type();
+}
+interface Carnivore {
+    void type();
+}
+
+class Bear implements Herbivore,Carnivore{
+    public void type(){
+        System.out.println("Bear is both a herbivore and a carnivore (omnivore)");
+    }
+}

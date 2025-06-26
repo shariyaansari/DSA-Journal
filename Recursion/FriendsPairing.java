cd90cb50c7526549cd7f658f8f648941f5a416fc
shariyaansari <shariyaansari8425@gmail.com>
2025-06-26 22:13:42 +0530

    Did 1 sum on recursion and 1 on array

---
+++ Recursion/FriendsPairing.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class FriendsPairing {
+    public static int pairFriends(int n){
+        // Base case 
+        if(n == 1 || n == 2){
+            return n;
+        }
+        return pairFriends(n-1) + (n-1) * pairFriends(n-2);
+    } 
+    public static void main(String[] args) {
+        System.out.println(pairFriends(5));
+    }
+}

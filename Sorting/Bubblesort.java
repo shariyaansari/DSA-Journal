d9ecd27f68ee26b8e93bf79a6e8f9bf86dff2c42
shariyaansari <shariyaansari8425@gmail.com>
2025-07-01 23:06:48 +0530

    added comments on the previously done algos

--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -1,22 +1,25 @@
 // https://leetcode.com/problems/pascals-triangle/description/
 package Sorting;
-
+// Has time complexity - O(n^2)
 public class Bubblesort {
     public static void sorting(int arr[]){
         int swap = 0;
         int n = arr.length;
         for(int i = 0; i < n - 1; i++){
             // swap = 0;
+            // n-1-i = if n = 6 and i = 1 then we will only need to peform swap on the first 5 elements cause swap targets first and next conscutive elements 
             for(int j = 0; j < n - 1 - i; j++){
                 if(arr[j] > arr[j+1]){
                     // swap
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    // Just to count the number of swaps it took to completely sort the array
                     swap++;
                 }
             }
         }
+        // This is for my convinience to print the array and the no of swaps it took to sort the array completely
         for(int i = 0; i < n; i++){
             System.out.println(arr[i]);
         }
--- Sorting/Insertion.java
+++ Sorting/Insertion.java
@@ -2,21 +2,23 @@ package Sorting;
 
 public class Insertion {
     public static void sorting(int[] arr){
-        int temp;
+        // int temp;
         int n = arr.length;
         for(int i = 1; i < n ; i++){
-            int curr = arr[i];
+            int curr = i;
             int prev = i - 1;
 
             // Finding out the correct pos to insert
-            while (prev >= 0 && arr[prev] > curr){
+            while (prev >= 0 && arr[prev] > arr[curr]){
                 arr[prev+1] = arr[prev];
                 prev--;
             }
             // insertion
-            arr[prev+1] = curr;
+            arr[prev+1] = arr[curr];
         }
-    
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i]);
+        }
     }
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};

b14ceaee57f6f4dbd730ee8deb4184e3aead8449
shariyaansari <shariyaansari8425@gmail.com>
2025-09-18 00:29:53 +0530

    Queues

--- Queue/QueueUsingStacks.java
+++ Queue/QueueUsingStacks.java
@@ -38,19 +38,52 @@ public class QueueUsingStacks {
         }
 
         // ======== Using POP =================
-        
-
+        public static void addUsingPop(int data){
+            // Directly push into s1 
+            s1.push(data);
+        }
+        public static int removeUsingPop(){
+            if (isEmpty()) return -1;
+            // to pop if s2 is empty then push all elements from s1 to s2
+            if (s2.isEmpty()) {
+                while(!s1.isEmpty()){
+                    s2.push(s1.pop());
+                }
+            }
+            // then return the top 
+            return s2.pop();
+        }
+        public static int peekUsingPop(){
+            if (isEmpty()) return -1;
+            // to pop if s2 is empty then push all elements from s1 to s2
+            if (s2.isEmpty()) {
+                while(!s1.isEmpty()){
+                    s2.push(s1.pop());
+                }
+            }
+            // then return the top 
+            return s2.peek();
+        }
     }
 
 
     public static void main(String[] args) {
-        Queue q = new Queue();
-        q.add(3);
-        q.add(6);
-        q.add(5);
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
+        // Queue q = new Queue();
+        // q.add(3);
+        // q.add(6);
+        // q.add(5);
+        // while (!q.isEmpty()) {
+        //     System.out.print(q.peek());
+        //     q.remove();
+        // }
+        System.out.println();
+        Queue q2 = new Queue();
+        q2.addUsingPop(3);
+        q2.addUsingPop(6);
+        q2.addUsingPop(5);
+        while (!q2.isEmpty()) {
+            System.out.println(q2.peekUsingPop());
+            q2.removeUsingPop();
         }
     }
 }

ac547aa27585fdbc970b2fb3723eeddc36b060e5
shariyaansari <shariyaansari8425@gmail.com>
2026-02-14 16:30:05 +0530

    Reorganizing DSA

--- Arrays/General/SubarraysWithXorK.java
+++
@@ -1,8 +0,0 @@
-package Arrays.General;
-
-public class SubarraysWithXorK {
-    public static void main(String[] args) {
-        System.out.println();
-        
-    }
-}
--- BitManipulation/converttolowercase.java
+++
@@ -1,8 +0,0 @@
-package BitManipulation;
-
-public class converttolowercase {
-    public static void main(String[] args) {
-        char ch = 'A'|32;
-        System.out.println(ch);
-    }
-}

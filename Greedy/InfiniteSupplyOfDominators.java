ee5abc8ac7e58ec1caee665fda2f7ee024314b40
Shariya Ansari <shariyaansari8425@gmail.com>
2026-04-28 03:10:58 +0530

    Greedy

--- Greedy/InfiniteSupplyOfDominators.java
+++ Greedy/InfiniteSupplyOfDominators.java
@@ -1,5 +1,21 @@
 package greedy;
+import java.util.*;
 
 public class InfiniteSupplyOfDominators {
-    
+    public static void count(int value, int[] arr){
+        List<Integer> res = new ArrayList<>();
+        while( value != 0){
+            for(int i = arr.length-1; i >= 0; i--){
+                if(value - arr[i] >= 0){
+                    value = value - arr[i];
+                    res.add(arr[i]);
+                }
+            }
+        }
+        System.out.println(res);
+    } 
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,10,50,100,500,1000}; 
+        count(121, arr);
+    }
 }

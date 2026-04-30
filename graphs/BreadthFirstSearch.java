f9493b716636ff4e4bfcf2593325586d13d2126e
Shariya Ansari <shariyaansari8425@gmail.com>
2026-05-01 00:46:05 +0530

    graphs

---
+++ graphs/BreadthFirstSearch.java
@@ -0,0 +1,73 @@
+package graphs;
+import java.util.*;
+
+public class BreadthFirstSearch {
+
+    static class Edge {
+
+        int src;
+        int destination;
+        int weight;
+
+        Edge(int s, int d, int w) {
+            this.src = s;
+            this.destination = d;
+            this.weight = w;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        // 0th vertex
+        graph[0].add(new Edge(0, 1, 5));
+        graph[0].add(new Edge(0, 2, 5));
+        // 1st vertex 
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 3, 1));
+        // 2nd vertex
+        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 4, 2));
+        // 3rd vertex
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+        // 4th vertex
+        graph[4].add(new Edge(4, 2, 2));
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 2));
+        // 5th
+        graph[5].add(new Edge(5, 3, 2));
+        graph[5].add(new Edge(5, 4, 2));
+        graph[5].add(new Edge(5, 6, 2));
+
+        // 6th
+        graph[6].add(new Edge(6, 5, 2));
+    }
+    // Time complexity = O(V+E)
+    public static void bfs(ArrayList<Edge>[] graph){
+        Queue<Integer> q = new LinkedList<>(); 
+        boolean[] visited = new boolean[graph.length];
+        q.add(0);  //taking source = 0
+        
+        while(!q.isEmpty()){
+            int curr = q.remove(); 
+            if(!visited[curr]){
+                System.out.print(curr + ",");
+                visited[curr] = true;
+                for(int i = 0; i < graph[curr].size();i++){
+                    Edge e = graph[curr].get(i); 
+                    q.add(e.destination);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Edge>[] graph = new ArrayList[7];
+        for (int i = 0; i < 7; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        createGraph(graph);
+        bfs(graph);
+        
+
+    }
+}

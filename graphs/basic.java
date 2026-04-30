904429023215706a9ab3a013e6ba77c2dbc42797
Shariya Ansari <shariyaansari8425@gmail.com>
2026-04-30 23:41:06 +0530

    Graphs

--- graphs/basic.java
+++ graphs/basic.java
@@ -10,7 +10,7 @@ public class basic {
             (1)/  \(1)
               /    \
              2------3
-            /
+            /   (1)
         (2)/
           4
     */
@@ -29,7 +29,28 @@ public class basic {
         int V = 5; 
         ArrayList<Edge>[] graph = new ArrayList[V];
         // ! Currently null stored in all the array list -> so make it an empty arraylist 
-        
-
+        for(int i = 0; i < V; i++){
+            graph[i] = new ArrayList<>();
+        }        
+        // 0th vertex
+        graph[0].add(new Edge(0, 1, 5));
+        // 1st vertex 
+        graph[1].add(new Edge(1,0,5));
+        graph[1].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1,3,1));
+        // 2nd vertex
+        graph[2].add(new Edge(2,4,2));
+        // 3rd vertex
+        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        // 4th vertex
+        graph[4].add(new Edge(4, 2, 2));
+        for(int i = 0; i < V;i++){
+            for(int j= 0; j < graph[i].size(); j++){
+                Edge e = graph[i].get(j);
+                System.out.println(e.source + ", "+ e.destination+", "+ e.weight);
+            }
+        }
     }
+        
 }
